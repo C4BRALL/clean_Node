eeca191bbb6ce78827846511d5ae8376502c8e52
Cabral <jcabral304@gmail.com>
2022-07-03 18:57:59 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- clean_Node/src/presentation/controllers/signUp.ts
+++ clean_Node/src/presentation/controllers/signUp.ts
@@ -1,11 +1,15 @@
+import { AddAccount } from "../../domain/usecases/add-account"
 import { InvalidParamError, MissingParamError } from "../errors"
 import { badRequest, serverError } from "../helpers/http-helper"
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from "../protocols"
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
-  constructor(emailValidator: any) {
+  private readonly addAccount: AddAccount
+
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle(httpRequest: HttpRequest): HttpResponse | undefined {
@@ -16,14 +20,22 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      const { email, password, passwordConfirmation } = httpRequest.body;
+
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
       if (password != passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
+
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError()
     }
--- clean_Node/src/presentation/controllers/signup.spec.ts
+++ clean_Node/src/presentation/controllers/signup.spec.ts
@@ -1,11 +1,8 @@
+import { AddAccount, AddAccountModel } from "../../domain/usecases/add-account";
+import { AccountModel } from "../../domain/usecases/models/account";
 import { InvalidParamError, MissingParamError, ServerError } from "../errors";
 import { EmailValidator } from "../protocols";
 import { SignUpController } from "./signUp";
-
-interface SutTypes {
-  sut: SignUpController
-  emailValidatorStub: EmailValidator
-}
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
@@ -14,7 +11,21 @@ const makeEmailValidator = (): EmailValidator => {
     }
   }
   return new EmailValidatorStub()
+}
 
+const makeAddAccount = (): AddAccount => {
+  class AddAccountStub implements AddAccount {
+    add(account: AddAccountModel): AccountModel {
+      const fakeAccount = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'valid_password'
+      }
+      return fakeAccount;
+    }
+  }
+  return new AddAccountStub();
 }
 
 // const makeEmailValidatorWithError = (): EmailValidator => {
@@ -26,13 +37,21 @@ const makeEmailValidator = (): EmailValidator => {
 //   return new EmailValidatorStub();
 // }
 
+interface SutTypes {
+  sut: SignUpController
+  emailValidatorStub: EmailValidator
+  addAccountStub: AddAccount
+}
+
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
-  const sut = new SignUpController(emailValidatorStub)
+  const addAccountStub = makeAddAccount()
+  const sut = new SignUpController(emailValidatorStub, addAccountStub)
 
   return {
     sut,
-    emailValidatorStub
+    emailValidatorStub,
+    addAccountStub
   }
 }
 
@@ -157,6 +176,25 @@ describe('Sign Up Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse?.statusCode).toBe(500);
     expect(httpResponse?.body).toEqual(new ServerError());
+  });
+
+  it('Should call AddAccount with correct values', () => {
+    const { sut, addAccountStub } = makeSut();
+    const addSpy = jest.spyOn(addAccountStub, 'add');
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'passphrase',
+        passwordConfirmation: 'passphrase'
+      }
+    }
+    sut.handle(httpRequest);
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'passphrase'
+    });
   });
 
 });
