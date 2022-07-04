4b66f429a5371a4c8b2b56819d2672084adf85f2
Joao Cabral de Medeiros Neto <joao.cabral@mobtelecom.com.br>
2022-07-04 17:24:52 -0300

    feat: ensure DbAddAccount returns an account success

--- clean_Node/src/data/usecases/add-account/db-add-account.spec.ts
+++ clean_Node/src/data/usecases/add-account/db-add-account.spec.ts
@@ -95,4 +95,20 @@ describe('DbAddAccount Usecase', () => {
     expect(promise).rejects.toThrow();
   })
 
+  it('Should return an account on success', async () => {
+    const { sut } = makeSut();
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_passphrase'
+    }
+    const account = await sut.add(accountData);
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'hashed_passphrase'
+    });
+  })
+
 })
