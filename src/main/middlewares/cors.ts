ee50c0feec7fc51876801e00df59bd53322937f5
João Cabral <jcabral304@gmail.com>
2022-07-08 11:29:03 -0300

    feat: ensure cors is enable

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.set('acces-control-allow-origin', '*')
-  
+  res.set('access-control-allow-origin', '*')
+  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-headers', '*')
   next()
 }
