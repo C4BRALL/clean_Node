58b422d4f21d5a07fdb4b47d805c374dd66d3d6b
João Cabral <jcabral304@gmail.com>
2022-07-08 14:05:32 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/singUp-routes.test.ts
+++ src/main/routes/singUp-routes.test.ts
@@ -1,7 +1,25 @@
 import request from "supertest";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
 
 describe('SingUp Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  // const makeSut = (): AccountMongoRepository => {
+  //   return new AccountMongoRepository()
+  // }
+
   it('Should return an account on success', async () => {
     await request(app)
       .post('/api/singUp')
