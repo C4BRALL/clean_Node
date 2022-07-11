cc173e64025af66c6795bee1be0573089965444f
João Cabral <jcabral304@gmail.com>
2022-07-11 15:23:34 -0300

    refactor: remove duplicated code

--- jest.config.js
+++ jest.config.js
@@ -4,29 +4,19 @@
  */
 
 module.exports = {
-  // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-
-  // The test environment that will be used for testing
-  testEnvironment: "node",
-
-  // Make sure mongodb is installed in the project as well, as it's required as a peer dependency.
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
-  
-  // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
