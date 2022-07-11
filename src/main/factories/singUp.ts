3b4a14e6d32e48b1b81bc710b7bf369041cd898e
João Cabral <jcabral304@gmail.com>
2022-07-11 15:49:19 -0300

    feat: ensure logControllerDecorator with correct error if controller returns a server error

--- src/main/factories/singUp.ts
+++ src/main/factories/singUp.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
+import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
 import { SignUpController } from "../../presentation/controllers/signup/signUp";
 import { EmailValidatorAdapter } from "../../utils/email-validator";
 import { LogControllerDecorator } from "../decorators/log";
@@ -12,5 +13,6 @@ export const makeSingUpController = (): SignUpController => {
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
