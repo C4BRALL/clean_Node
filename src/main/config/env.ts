eaab6f0d8dc3cc8c5a7de4423a019ae2f5d98897
João Cabral <joao.cabral@mobtelecom.com>
2022-08-09 18:12:01 -0300

    feat: ensure add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb+srv://cabralMob:0KYtp0cg7hrmiWz3@clustercenter.kwe8j.mongodb.net/clean-node-api?retryWrites=true&w=majority',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'dsdwn==5h'
 }
