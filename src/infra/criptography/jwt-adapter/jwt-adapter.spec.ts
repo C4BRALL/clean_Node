efe2a279b20a37935ac9be9d8a0d6eea625bcf04
João Cabral <joao.cabral@mobtelecom.com>
2022-08-09 09:35:13 -0300

    test: ensure Jwt Adapter return a valid sign method on success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,15 +1,27 @@
 import jwt from 'jsonwebtoken';
 import { JwtAdapter } from './JwtAdapter';
 
+jest.mock('jsonwebtoken', () => ({
+  async sign(): Promise<string> {
+    return new Promise(resolve => resolve('any_token'))
+  }
+}))
+
 const makeSut = (): JwtAdapter => {
   return new JwtAdapter('secretKey')
 }
 
 describe('Jwt Adapter', () => {
-  it('Shoul call sign with correct values', () => {
+  it('Should call sign with correct values', async () => {
     const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({id: 'any_id'}, 'secretKey')
+  })
+
+  it('Should return a valid sign on success', async () => {
+    const sut = makeSut()
+    const validSign = await sut.encrypt('any_id')
+    expect(validSign).toBe('any_token')
   })
 })
