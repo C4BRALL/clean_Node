5ba681b80debf09fc13e89ad514b853073522c9d
Cabral <jcabral304@gmail.com>
2022-08-25 21:41:17 -0300

    chore: connect mongoDB Atlas to Heroku var

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://cabralMob:0KYtp0cg7hrmiWz3@clustercenter.kwe8j.mongodb.net/clean-node-api?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL,// || 'mongodb+srv://cabraldev:HjrKZg6mtYhsC9mr@clustercenter.kwe8j.mongodb.net/clean-node-api?retryWrites=true&w=majority',
   port: process.env.PORT || 5356,
   jwtSecret: process.env.JWT_SECRET || 'dsdwn==5h'
 }
